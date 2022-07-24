2bfb2349152d116585a8815074fb45d4c998d15f
smart-systems <73495913+smrtsstms@users.noreply.github.com>
2022-07-24 11:31:35 +0300

    Add files via upload

---
+++ ecolab-runner.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+### BEGIN INIT INFO
+# Provides:          smartsys
+# Required-Start:    $all
+# Required-Stop:     $local_fs $remote_fs
+# Should-Start:      $syslog
+# Should-Stop:       $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start or stop.
+### END INIT INFO
+
+sudo adduser $USER sudo
+sudo adduser $USER dialout
+
+sudo cp 99-usb.rules /etc/udev/rules.d/
+sudo udevadm trigger
+
+export CR_PAT=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
+echo $CR_PAT | docker login ghcr.io -u $USER --password-stdin
+
+docker volume create --name=ecolab_postgres-data
+docker volume create --name=grafana-data
+
+docker pull ghcr.io/smrtsstms/ecolab_master:latest
+docker pull ghcr.io/smrtsstms/ecolab_device_connector:latest
+docker pull ghcr.io/smrtsstms/ecolab_db_connector:latest
+docker pull ghcr.io/smrtsstms/ecolab_grafana:latest
+
+docker-compose up
+
+xdg-open http://192.168.1.130:9000/d/dLmLK0gVk/panel-pokazanii-s-pribora-ekolab?orgId=1
